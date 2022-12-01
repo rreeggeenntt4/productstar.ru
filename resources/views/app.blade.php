ed680765d507fb1c80332fd31bacf333dd05cb99
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2022-12-01 17:32:37 +0300

    install inertia && vue 3 and and other dependencies
    Ранее комментарии по установке можно пропустить и переходить сразу к этому,  для установки основной связки laravel9 vite && inertia && vue
    
    Инструкция https://bootcamp.laravel.com/inertia/installation
    
    composer require laravel/breeze --dev
    php artisan breeze:install vue
    
    в помощь https://www.youtube.com/watch?v=VwC6T-udjas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,20 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  @vite('resources/js/app.js')
-</body>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
